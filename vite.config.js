482b6a240da54474230e5bf269214f6644e145d2
MohammedKhred7 <mhmdkhrd9@gmail.com>
2024-05-29 05:40:37 +0300

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // base: "/Photo-Gallery-vue.js/",
+  base: "/Photo-Gallery-using-vue.js/",
   resolve: {
     alias: [
         {find: "@", replacement: path.resolve(__dirname, 'src')}
